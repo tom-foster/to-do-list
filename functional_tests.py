a1d360871a26f0e5887c41074cc084b7188fb42e
ohyouknowwho <theresidentdj@hotmail.com>
2016-03-06 21:00:15 +0000

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,43 @@
 #Created first function test in python.
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
-#Makes a test assertion (assumption) that Django should be listed in the title
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+        def setUp(self):
+            self.browser = webdriver.Firefox()
+            self.browser.implicitly_wait(3)
+
+        def tearDown(self):
+            self.browser.quit()
+
+        def test_can_start_a_list_and_retrieve_it_later(self):
+            #Mary has heard about a cool new online to-do app. She goes to check out it's
+            #homepage.
+            self.browser.get("http://localhost:8000")
+            #She notices ithe page title and header mention to-do lists
+            self.assertIn('To-Do', self.browser.title)
+            self.fail('Finish the test!')
+            #She is invited to enter a to-do item straight away
+
+            # She types "Buy peacock feathers" into a text box (Mary's hobby is tying
+            # fly-fishing lures)
+
+            #When she hits enter, the page updates, and now the page lists
+            #"1: Buy peacock feathers" ans an  item in a to-do list
+
+            #There is still a text box inviting her to add another item. She enters
+            #"Use peacock feahters to make a fly" (Mary is very methodical)
+
+            #The page updates again, and now shows both items on her list
+
+            #Mary wonders whether the site will remember her list. Then she sees that the
+            #site has generated a unique URL for her -- there is some explanatory text to
+            # that effect.
+
+            #She visits that URL - her to-do list is still there.
+
+            #Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
