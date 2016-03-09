5e0e9eaae203fd9c4a014522e8d17b4b78ed853a
tom-foster <tom-foster@users.noreply.github.com>
2016-03-09 16:50:37 +0000

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 #Created first function test in python.
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,18 +18,29 @@ class NewVisitorTest(unittest.TestCase):
             self.browser.get("http://localhost:8000")
             #She notices ithe page title and header mention to-do lists
             self.assertIn('To-Do', self.browser.title)
-            self.fail('Finish the test!')
+            header_test = self.browser.find_element_by_tag_name('h1').text
+            self.assertIn = ('To-Do', header_text)
             #She is invited to enter a to-do item straight away
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertEqual(inputbox.get_attribute('placeholder'),
+                             'Enter a to-do item')
+
 
             # She types "Buy peacock feathers" into a text box (Mary's hobby is tying
             # fly-fishing lures)
-
+            inputbox.send_keys('Buy peacock feathers')
             #When she hits enter, the page updates, and now the page lists
-            #"1: Buy peacock feathers" ans an  item in a to-do list
+            #"1: Buy peacock feathers" as an item in a to-do list
+            inputbox.send_keys(Keys.ENTER)
 
+            table = self.browser.find_element_by_id('id_list_table')
+            rows = table.find_elements_by_tag_name('tr')
+            self.assertTrue(
+                        any(row.text == '1: Buy peacock feathers' for row in rows)
+            )
             #There is still a text box inviting her to add another item. She enters
             #"Use peacock feahters to make a fly" (Mary is very methodical)
-
+            self.fail('Finish the test')
             #The page updates again, and now shows both items on her list
 
             #Mary wonders whether the site will remember her list. Then she sees that the
