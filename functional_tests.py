6a756a550cc4b5bbee26a7144f1a57c01e22976a
tom-foster <tom-foster@users.noreply.github.com>
2016-03-10 17:10:21 +0000

    Changes before functionals tests refactor

--- functional_tests.py
+++ functional_tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
             #She notices ithe page title and header mention to-do lists
             self.assertIn('To-Do', self.browser.title)
             header_text = self.browser.find_element_by_tag_name('h1').text
-            self.assertIn = ('To-Do', header_text)
+            self.assertIn('To-Do', header_text)
             #She is invited to enter a to-do item straight away
             inputbox = self.browser.find_element_by_id('id_new_item')
             self.assertEqual(inputbox.get_attribute('placeholder'),
@@ -35,19 +35,26 @@ class NewVisitorTest(unittest.TestCase):
 
             table = self.browser.find_element_by_id('id_list_table')
             rows = table.find_elements_by_tag_name('tr')
-            self.assertTrue(
-                        any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table"
-            )
+
+            self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
             #There is still a text box inviting her to add another item. She enters
             #"Use peacock feahters to make a fly" (Mary is very methodical)
-            self.fail('Finish the test')
-            #The page updates again, and now shows both items on her list
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox.send_keys('Use peacock feathers to make a fly')
+            inputbox.send_keys(Keys.ENTER)
 
+            #The page updates again, and now shows both items on her list
+            table = self.browser.find_element_by_id('id_list_table')
+            rows = table.find_elements_by_tag_name('tr')
+            self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+            self.assertIn(
+                        '2: Use peacock feathers to make a fly',
+                        [row.text for row in rows]
+            )
             #Mary wonders whether the site will remember her list. Then she sees that the
             #site has generated a unique URL for her -- there is some explanatory text to
             # that effect.
-
+            self.fail('Finish the test')
             #She visits that URL - her to-do list is still there.
 
             #Satisfied, she goes back to sleep
