886c8634d8ae681a0819c8e965fbfe2d41e70af5
tom-foster <tom-foster@users.noreply.github.com>
2016-03-15 23:59:27 +0000

    Changing Models

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#List class needs to be declared prior to it's assigning in Item
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,8 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item, List
 from lists.views import home_page
-from lists.models import Item
 
 class HomePageTest(TestCase):
     """A test to see if the homepage is there"""
@@ -18,16 +18,23 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-class ItemModelTest(TestCase):
-
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -35,12 +42,15 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class LiveViewTest(TestCase):
     def test_display_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -55,7 +65,7 @@ class NewListTest(TestCase):
         def test_saving_a_POST_request(self):
             self.client.post(
                             '/lists/new',
-                            data={'item_text' : 'A new list item'}
+                            data={'item_text' : 'A new list item'},
             )
 
             self.assertEqual(Item.objects.count(), 1)
