c1f9468850bd00347964d64edc640c18af9d8786
tom-foster <tom-foster@users.noreply.github.com>
2016-04-10 23:08:09 +0100

    rename all item input ids and names. still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -14,7 +14,7 @@ class NewVisitorTest(FunctionalTest):
             header_text = self.browser.find_element_by_tag_name('h1').text
             self.assertIn('To-Do', header_text)
             #She is invited to enter a to-do item straight away
-            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox = self.browser.find_element_by_id('id_text')
             self.assertEqual(inputbox.get_attribute('placeholder'),
                              'Enter a to-do item')
 
@@ -30,7 +30,7 @@ class NewVisitorTest(FunctionalTest):
             self.check_for_row_in_list_table('1: Buy peacock feathers')
             #There is still a text box inviting her to add another item. She enters
             #"Use peacock feahters to make a fly" (Mary is very methodical)
-            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox = self.browser.find_element_by_id('id_text')
             inputbox.send_keys('Use peacock feathers to make a fly')
             inputbox.send_keys(Keys.ENTER)
 
@@ -51,7 +51,7 @@ class NewVisitorTest(FunctionalTest):
             self.assertNotIn('make a fly', page_text)
             #Rachael starts a new list by entering a new item. She is
             #less interesting than Mary...
-            inputbox = self.browser.find_element_by_id('id_new_item')
+            inputbox = self.browser.find_element_by_id('id_text')
             inputbox.send_keys('Buy milk')
             inputbox.send_keys(Keys.ENTER)
             #Rachael gets her own unique URL
