cd6c48ba2b15f942c49b7349804eea7e7f0c00e4
tom-foster <tom-foster@users.noreply.github.com>
2016-04-14 22:06:56 +0100

    Updated user story and functional test for client side

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,26 +4,29 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+        def get_error_element(self):
+            return self.browser.find_element_by_css_selector('.has-error')
+
         def test_cannot_add_empty_list_items(self):
             #Rachael goes to the home page and accidentally tries to submit
             #an empty list item. She hits Enter on the the empty input box
             self.browser.get(self.server_url)
-            self.browser.find_element_by_id('id_text').send_keys('\n')
+            self.get_item_input_box().send_keys('\n')
             #The home page refreshes, and there is an error message saying
             #that list items cannot be blank.
-            error = self.browser.find_element_by_css_selector('.has-error')
+            error = self.get_error_element()
             self.assertEqual(error.text, "You can't have an empty list item")
             #She tries again with some text for the item, which now works
-            self.browser.find_element_by_id('id_text').send_keys('Buy milk\n')
+            self.get_item_input_box().send_keys('Buy milk\n')
             self.check_for_row_in_list_table('1: Buy milk')
             #Perversely, she now decides to submit a second blank list item
             self.browser.find_element_by_id('id_text').send_keys('\n')
             #She receives a similar warning on the list page
             self.check_for_row_in_list_table('1: Buy milk')
-            error = self.browser.find_element_by_css_selector('.has-error')
+            error = self.get_error_element()
             self.assertEqual(error.text, "You can't have an empty list item")
             #And she can correct it by filling some text in
-            self.browser.find_element_by_id('id_text').send_keys('Make tea\n')
+            self.get_item_input_box().send_keys('Make tea\n')
             self.check_for_row_in_list_table('1: Buy milk')
             self.check_for_row_in_list_table('2: Make tea')
 
@@ -38,8 +41,23 @@ class ItemValidationTest(FunctionalTest):
 
             #She sees a helpful error_message
             self.check_for_row_in_list_table('1: Buy wellies')
-            error = self.browser.find_element_by_css_selector('.has-error')
+            error = self.get_error_element()
             self.assertEqual(error.text, "You've already got this in your list")
+
+        def test_error_messages_are_cleared_on_input(self):
+            #Mary starts a new list in a way that causes a validation error:
+            self.browser.get(self.server_url)
+            self.get_item_input_box().send_keys('\n')
+            error = self.get_error_element()
+            self.assertTrue(error.is_displayed())
+
+            #She starts typing in the input box to clear the error
+            self.get_item_input_box().send_keys('a')
+
+            #She is pleased to see that the error message disappears
+            error = self.get_error_element()
+            self.assertFalse(error.is_displayed())
+
 
 if __name__ == "__main__":
     unittest.main()
