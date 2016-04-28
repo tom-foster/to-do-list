822caf7fba7fbf5fc3010bac9e0ea68fee581c10
tom-foster <tom-foster@users.noreply.github.com>
2016-04-28 23:57:26 +0100

    Patched at a class level in Authenticate Test

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,4 +1,7 @@
 import requests
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 DOMAIN = 'localhost'
@@ -7,7 +10,9 @@ DOMAIN = 'localhost'
 class PersonaAuthenticationBackend(object):
 
       def authenticate(self, assertion):
-        requests.post(
+        response = requests.post(
             PERSONA_VERIFY_URL,
             data={'assertion': assertion, 'audience': DOMAIN}
         )
+        if response.ok and response.json()['status'] == 'okay':
+            return User.objects.get(email=response.json()['email'])
