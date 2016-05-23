87a318ab451ab2a1d90709de535ce534be75c7e2
tom-foster <tom-foster@users.noreply.github.com>
2016-05-23 23:59:48 +0100

    windows fab file another alteration

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -16,7 +16,7 @@ def create_session_on_server(host, email):
 
 def reset_database(host):
     subprocess.check_call(
-        ['C:/Python27/Scripts/fab.exe', 'reset_database', 'host={}'.format(host),
+        ['C:/Python27/Scripts/fab.exe', 'reset_database', '--host={}'.format(host),
         "-i C:/Users/Tom/Documents/website_projects/django/key_pair/first-server-open",
         ],
         cwd=THIS_FOLDER
