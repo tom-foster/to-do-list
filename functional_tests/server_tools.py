b866358ed0976542fb2d8bd4931a7ff6c7501547
tom-foster <tom-foster@users.noreply.github.com>
2016-05-25 22:41:50 +0100

    Corrected second test for fab windows

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -6,10 +6,12 @@ THIS_FOLDER = path.dirname(path.abspath(__file__))
 def create_session_on_server(host, email):
     return subprocess.check_output(
         [
-            'fab',
+            'C:/Python27/Scripts/fab.exe',
             'create_session_on_server:email={}'.format(email),
             '--host={}'.format(host),
             '--hide=everything,status',
+            '-i',
+            'C:/Users/Tom/Documents/website_projects/django/key_pair/first-server-open'
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
