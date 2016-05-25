1136ba2c0417b5f47d8174368387c5070ac4d30c
tom-foster <tom-foster@users.noreply.github.com>
2016-05-25 22:33:25 +0100

    Final attempt to fix Fabric file for Windows sub process

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -13,9 +13,10 @@ def create_session_on_server(host, email):
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
-
+#This hangs on live pages 319 onwards are broke on windows.
 def reset_database(host):
     subprocess.check_call(
-        ['C:/Python27/Scripts/fab.exe', 'reset_database', '--host={}'.format(host)],
+        ['C:/Python27/Scripts/fab.exe', 'reset_database', '--host={}'.format(host),
+        '-i', 'C:/Users/Tom/Documents/website_projects/django/key_pair/first-server-open'],
         cwd=THIS_FOLDER
     )
