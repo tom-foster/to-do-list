8488d900c00856a996d4d8244c808e2a8ef9554a
tom-foster <tom-foster@users.noreply.github.com>
2016-06-08 23:25:34 +0100

    add screenshot on falure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,7 +3,13 @@ from .server_tools import reset_database
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+import os
+from datetime import datetime
 import sys
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -29,12 +35,47 @@ class FunctionalTest(StaticLiveServerTestCase):
             if self.against_staging:
                 reset_database(self.server_host)
             # self.browser = webdriver.Chrome('C:/Users/Tom\Documents/website_projects/django/chromedriver_win32/chromedriver')
-            self.browser = webdriver.Firefox() 
+            self.browser = webdriver.Firefox()
             self.browser.implicitly_wait(3)
 
         def tearDown(self):
-            self.browser.refresh()
+            if self._test_has_failed():
+                if not os.path.exists(SCREEN_DUMP_LOCATION):
+                    os.makedirs(SCREEN_DUMP_LOCATION)
+                for ix, handle in enumerate(self.browser.window_handles):
+                    self._windowid = ix
+                    self.browser.switch_to_window(handle)
+                    self.take_screenshot()
+                    self.dump_html()
             self.browser.quit()
+            super().tearDown()
+
+        def _test_has_failed(self):
+            for method, error in self._outcome.errors:
+                if error:
+                    return True
+            return False
+
+        def take_screenshot(self):
+            filename = self._get_filename() + '.png'
+            print('screenshotting to', filename)
+            self.browser.get_screenshot_as_file(filename)
+
+        def dump_html(self):
+            filename = self._get_filename() + '.html'
+            print('dumping page HTML to', filename)
+            with open(filename, 'w') as f:
+                f.write(self.browser.page_source)
+
+        def _get_filename(self):
+            timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+            return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+                folder=SCREEN_DUMP_LOCATION,
+                classname=self.__class__.__name__,
+                method=self._testMethodName,
+                windowid=self._windowid,
+                timestamp=timestamp
+            )
 
         def check_for_row_in_list_table(self, row_text):
             table = self.browser.find_element_by_id('id_list_table')
